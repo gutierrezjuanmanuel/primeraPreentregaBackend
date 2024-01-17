6105114e85b24bc33eda048d25b91b2d931a8011
Juan Manuel Gutierrez <gutierrezmanueljuan@outlook.com>
2024-01-17 18:00:52 -0300

    cambio

--- src/app.js
+++ src/app.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
-const PUERTO = 8080; 
+const PUERTO = 8080;
 const productsRouter = require("./routes/products.router.js");
 const cartsRouter = require("./routes/carts.router.js");
 
 //Middleware
-app.use(express.urlencoded({extended: true}));
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Routing
+//Rutas: 
 app.use("/api", productsRouter);
 app.use("/api", cartsRouter);
 
-//Listen 
+
 
 app.listen(PUERTO, () => {
-    console.log(`Servidor escuchando en el puerto http://localhost: ${PUERTO}`);
+    console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
